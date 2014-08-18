69156d050b8c65e7df82e7e1080c1c098875e969
Dmitry Les <dmitriy.s.les@gmail.com>
2014-08-18 19:34:28 +0300

    initial

---
+++ example/app.js
@@ -0,0 +1,47 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    expressAsyncValidator = require('../lib/express_async_validator');
+
+var app = express();
+app.use(bodyParser());
+app.use(expressAsyncValidator());
+
+app.set('port', 3000);
+
+require('http').createServer(app).listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+});
+
+app.all('*', function (req, res, next) {
+    req
+        .validate({
+            "limit":  {
+                "rules":   {
+                    "required": {
+                        "message": "Limit is required"
+                    }
+                },
+                "options": {
+                    "source": ["query"]
+                }
+            },
+            "offset": {
+                "rules":   {
+                    "required": {
+                        "message": "Offset is required"
+                    }
+                },
+                "options": {
+                    "source": ["query"]
+                }
+            }
+        })
+        .then(function (model) {
+            res.status(200).json(model);
+        })
+        .catch(function (error) {
+            res.status(400).json(error);
+        });
+});
+
+module.exports = app;
